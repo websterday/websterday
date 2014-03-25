8aba10735ecd230b32232dc11068c00c75bbc658
Aurélien Praga <aurelien.praga@gmail.com>
2014-03-25 16:00:40 +0100

    Add user

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -1,16 +1,38 @@
 'use strict';
 
 angular.module('bookmarksApp')
-	.controller('UserAddCtrl', ['$scope', function ($scope) {
+	.controller('UserAddCtrl', ['$scope', 'growl', 'User', function ($scope, growl, User) {
 
+		$scope.user = {};
+
+		$scope.showConfirmation = false;
+
+		$scope.create = function() {
+			if ($scope.user.username != null && $scope.user.email != null && $scope.user.password != null &&
+				$scope.user.confirmPassword != null) {
+				if ($scope.user.password == $scope.user.confirmPassword) {
+					delete $scope.user.confirmPassword;
+					// console.log($scope.user);
+					User.post({user: $scope.user}, function(data) {
+						if (angular.isDefined(data[0]) && data[0] == 1) {
+							$scope.showConfirmation = true;
+						}  else if (angular.isDefined(data.error)) {
+							growl.addErrorMessage(data.error);
+						}
+					});
+				} else {
+					growl.addErrorMessage('Passwords are different');
+				}
+			} else {
+				growl.addErrorMessage('All fields are required');
+			}
+		}
 	}])
 	.controller('UserLoginCtrl', ['$scope', '$cookieStore', '$location', 'User', function ($scope, $cookieStore, $location, User) {
 		$scope.showError = false;
 
 		$scope.login = function(username) {
 			$scope.showError = false;
-
-			console.log(username);
 
 			if ($scope.username != null && $scope.password != null) {
 				// console.log('login');
